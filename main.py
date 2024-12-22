c77f21289d417650fa8d771a686ffa20d721b9d6
Daniel <123036824+danroke@users.noreply.github.com>
2024-12-22 21:24:42 +0000

    initial

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-#starter
+# regex module
+import re
+
+
+## input word search is 140x140
+
+with open('input.txt', 'r') as file:
+    for line in file:    
+
+        pattern = r"XMAS"
+        matches = re.findall(pattern, line)
+        
+        
