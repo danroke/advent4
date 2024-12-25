d29420114b5b3891232d5277000d9563b336b104
Daniel <123036824+danroke@users.noreply.github.com>
2024-12-25 22:36:57 +0000

    WIP

--- main.py
+++ main.py
@@ -8,15 +8,40 @@ rows = 140
 word_search_array = [0*cols]*rows # create a 140*140 list object filled with 0's
 line_counter = 1
 char_counter = 0
+# count number of 'XMAS' matches
+match_counter = 0
 
-print(word_search_array)
-
+# Read in text
 with open('input.txt', 'r') as file:
     for line in file:
-        for character in line:   
-            word_search_array.insert(char_counter*line_counter, character)
-            char_counter += 1
+        for character in line:
+            if character != "\n":   
+                word_search_array.insert(char_counter*line_counter, character)
+                char_counter += 1
         line_counter += 1
-print(word_search_array)
+
+# Look for the next occurance of 'X'
+# Check each cardinal direction
+char_counter = 0
+for character in word_search_array:
+    char_counter += 1 
+    if character == "X":
+        # E
+        if word_search_array[char_counter + 1] == "M":
+            if word_search_array[char_counter + 2] == "A":
+                if word_search_array[char_counter + 3] == "S": 
+                    match_counter += 1
+
+    # S
+# W
+# N
+
+# diagonals
+# NE
+# SE
+# SW
+# NW
+
+print(match_counter)
         
         
